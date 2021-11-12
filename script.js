64faaeab773cdaf44d7d0896e1bd38f46a6a3fde
Jadeilson.Santos <jadeilsontechnology@gmail.com>
2021-11-12 15:54:48 -0300

    dias de folga corrigido

--- script.js
+++ script.js
@@ -1,84 +1,71 @@
 var anoAtual = new Date().getFullYear(); 
 var folgaBase = new Date('11/04/2021');
 const umDiaEmMS = 86400000
-var texto = document.querySelector('.resultado');
+var texto = document.querySelector('.resultado h3');
+var ndata;
 
-/* const convertDate = () => {
-  
-  //e aí manoo
-  //ok
-  
-  let day = dataSelecionada.getDate();
-  let moth = dataSelecionada.getMoth();
-  let year = dataSelecionada.getFullYear();
-  
-  dataSelecionada = `${day}/${moth}/${year}`
-  
-  minhaFolga(e,dataSelecionada,selectDate) 
-}  */
 
 function minhaFolga(e){
   
   let lista
+  apagar(lista);
   
   const dataSelecionada = e.target.value;
   selectDate = new Date(dataSelecionada)
   
   let diasDeFolga = [];
   let folgaBaseAdd = new Date(folgaBase);
-  //let i = 0;
   
   while (folgaBaseAdd <= selectDate){ 
-    diasDeFolga.push(folgaBaseAdd.toString()); // aqui deve ta retornando sempre a mesma data        
+    diasDeFolga.push(folgaBaseAdd.toString());    
     folgaBaseAdd.setDate(folgaBaseAdd.getDate()+8)
-    //i+=8 // aqui faz o incremento da data né? >>> Sim
+  
   }
   
   
   let diferenca = Math.floor((folgaBaseAdd - selectDate) / umDiaEmMS)
   
-  /* if(diferenca > 0 && diferenca <5){
+    if(diferenca > 0 && diferenca <5){
     texto.innerHTML = (`Você estará de FOLGA no dia ${ converterDatas(dataSelecionada) }`)
-  }else{
+  } else{
     texto.innerHTML += (`Você TRABALHARÁ nos dia ${converterDatas(dataSelecionada)}`)
-  } */
-  apagar(lista);
+  } 
+ 
 
-var dados = document.createElement('li') // cria elemento li
+  var dados = document.createElement('li') // cria elemento li
   
   diasDeFolga.forEach(dias => {
-      lista = document.querySelector('.lista').appendChild(dados)
-      lista.innerHTML += `| ${converterDatas(dias)}` 
-    })
-   
-  
-  //console.log(diferenca)
-      //console.log(diasDeFolga)
-}
+    lista = document.querySelector('.lista').appendChild(dados)
+      for (let index = 4; index < 8; index++) {
 
+        lista.innerHTML += `| ${(converterDatas(dias, index))} `
+      } 
+    })
+}
 function apagar(li){
     li = document.querySelector(".lista")
     li.innerHTML = ""
+    texto.innerHTML = ""
 }
 
 function zeroNaFrente(data){
-  return data <= 9 ? `0${data}`: `${data}` 
+  return data <10 ? `0${data}`: `${data}` 
 }
 
-function converterDatas(d){    
+function setarData(d,i){
   let data = new Date(d)
-  let ndata = new Date(data)
-  ndata.setDate(data.getDate() + 1)
- 
-  let dia = ndata.getDate()
-  let mes = ndata.getMonth() +1
-  let ano = ndata.getFullYear()
+  ndata = new Date(data)
+ ndata.setDate(data.getDate() + i)
+return data;
+}
 
-  //console.log(`${dia}/${mes}/${ano}`)
-  //return ndata
+function converterDatas(d,i=1){    
+  setarData(d,i); 
+  let dia = ndata.getDate();
+  let mes = ndata.getMonth() + 1;
+  let ano = ndata.getFullYear();
+
   return `${zeroNaFrente(dia)}/${zeroNaFrente(mes)}/${ano}` // aqui altera como vc quer o retorno
 }
 
-// Vou jogar aqui o codigo
-// Testa ai
 
